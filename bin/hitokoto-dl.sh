a4c624c57ed6bcd302ff90ac69d8ee91cd242aaf
kazuhikoh <kazuhiko.hrmt@gmail.com>
2021-03-29 18:28:19 +0900

    (⊃＾ω＾)⊃ improve hitokoto-dl output

--- bin/hitokoto-dl.sh
+++ bin/hitokoto-dl.sh
@@ -2,4 +2,17 @@
 
 : $1 $2
 
-gmr music-album -p $1 $2 | awk '{printf "curl %s > %s.mp3\n", $1, $2}' | xargs -I{} bash -c '{}'
+function dl() {
+  url="$1"
+  out="${2}.mp3"
+
+  if [[ -e "$out" ]]; then
+    echo "SKIP $out"
+  else
+    echo "GET  $out"
+    curl -Ss "$url" > "$out"
+  fi
+}
+
+export -f dl
+gmr music-album -p $1 $2 | xargs -I{} bash -c 'dl {} {}'
