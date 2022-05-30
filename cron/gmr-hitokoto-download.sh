d9de94872a6f357b27b52bf328eec8741a5d21d9
kazuhikoh <kazuhiko.hrmt@gmail.com>
2022-05-31 01:19:10 +0900

    (⊃＾ω＾)⊃ refactor gmr hitokoto download

--- cron/gmr-hitokoto-download.sh
+++ cron/gmr-hitokoto-download.sh
@@ -5,23 +5,40 @@ PATH=$PATH:~/bin
 PATH=$PATH:~/.npm-global/bin
 
 readonly DIR_ROOT="$1"
-readonly SLACKPOST_ID="$2"
+readonly SLACKPOST_ID=$2
 
-mkdir "$DIR_ROOT"
 {
   cd "$DIR_ROOT"
+
   gmr music-albums -p 0 0 2 \
     | sed 's/ /_/g' \
     | sed 's/_/ /' \
     | sed 's/_/ /' \
-    | xargs -n3 bash -c 'echo $2; mkdir -p $2; cd $2; hitokoto-dl $0 $1;'
+    | while read id1 id2 title
+      do
+        echo "$title"
+        (
+          mkdir -p "$title"
+          cd "$title"
+
+          gmr music-album -p $id1 $id2 \
+            | while read url track
+              do
+                out="${track}.mp3"
+                if [[ -e "$out" ]]; then
+                  echo "SKIP $out"
+                else
+                  echo "GET  $out"
+                  curl -Ss "$url" > "$out"
+                fi
+              done
+        )
+      done
 } >&2
 
+# Output
 {
   cd "$DIR_ROOT" 
   ls -1 | xargs -I{} bash -c "echo -n {}' '; ls {} | wc -l"
 } | slack-post -w "$SLACKPOST_ID"
-
-
-
 
