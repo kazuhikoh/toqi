7d3d403591acd9dd52ec80cd192a58d6b9f62315
kazuhikoh <kazuhiko.hrmt@gmail.com>
2023-04-18 01:42:23 +0900

    (⊃＾ω＾)⊃ agrec check

--- cron/record-agqr-check.sh
+++ cron/record-agqr-check.sh
@@ -3,10 +3,27 @@
 PATH=$PATH:/usr/local/bin
 PATH=$PATH:~/bin
 
-readonly SLACKPOST_ID="$1"
+readonly ERROR_FILEPATH="${1:-"/tmp/agrec-check.err"}"
+readonly SLACKPOST_ID="$2"
 
-out=$(agrec -c)
+readonly ERROR_FILEPATH_SUFFIX="-$(date '+%Y%m%d%H%M%S')"
+
+# skip if error file exists
+dir="${ERROR_FILEPATH%/*}"
+f="${ERROR_FILEPATH##*/}"
+if ls "$dir"| grep "$f" >/dev/null; then
+  exit 0
+fi
+
+# agrec check
+out="$(agrec -c)"
 
 if [ $? -ne 0 ]; then
-  { echo "(⊃＾ω＾)⊃ AGQR RECORDING CHECK"; echo "$out"; } | slack-post -w "$SLACKPOST_ID"
+  echo "$out" >"${ERROR_FILEPATH}${ERROR_FILEPATH_SUFFIX}"
+
+  {
+    echo "(⊃＾ω＾)⊃ AGQR RECORDING CHECK"
+    echo "$out"
+  } | slack-post -w "$SLACKPOST_ID"
 fi
+
